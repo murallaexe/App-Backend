fd3808af23af267c3308129eef6c58824cfc310f
murallaexe <joksanhaniel@gmail.com>
2021-12-10 23:30:19 -0600

    PORTS

--- modules/database.js
+++ modules/database.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose');
 
 let bd = 'VejaDelivery';
 let port = '27017';
-let host = 'localhost';
+let host = 'vejadelivery.herokuapp.com';
 
 class Database{
     constructor(){
         this.conectar();
     }
     conectar(){
-        mongoose.connect(`mongodb://mongodb+srv://muralla98:Ramosjaniel98.@cluster0.unaj4.mongodb.net/test`)
+        mongoose.connect(`mongodb://${host}:${port}/${bd}`)
         .then(result=>console.log('se conecto a mongodb'))
         .catch(error=>console.log(error));
         //aqui
